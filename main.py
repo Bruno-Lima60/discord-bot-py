f220e02a4849b6cc27a08b44a34279b95db7260b
bruno <brunolima26075@gmail.com>
2025-07-19 16:59:22 -0300

    arquivos adicionados

---
+++ main.py
@@ -0,0 +1,36 @@
+import discord
+import os
+import logging
+from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
+
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+logging.basicConfig(level=logging.INFO, handlers=[handler])
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+
+bot = commands.Bot(command_prefix='.', intents=intents, help_command=None)
+
+@bot.event
+async def on_ready():
+    print(f"Ready!")
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+    
+    await bot.process_commands(message)
+
+async def main():
+    token = os.getenv('DISCORD_TOKEN')
+    if token is None:
+        raise ValueError("Token environment variable not set!")
+
+    await bot.start(token)
+
+import asyncio
+asyncio.run(main())
